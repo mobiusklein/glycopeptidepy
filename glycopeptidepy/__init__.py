567ce03eb39243324bfd08efff8b3114e4f74bf8
Joshua Klein <mobiusklein@gmail.com>
2018-03-02 13:28:40 -0500

    add better structure to top-level available names

--- glycopeptidepy/__init__.py
+++ glycopeptidepy/__init__.py
@@ -1,5 +1,23 @@
 from .structure.sequence import PeptideSequence, ProteinSequence
-from .structure.modification import Modification
-from .structure.residue import Residue as AminoAcidResidue
+from .structure.modification import (
+    Modification, ModificationTable, RestrictedModificationTable,
+    ModificationRule, ModificationTarget, AnonymousModificationRule)
+from .structure.fragment import (
+    IonSeries, PeptideFragment, ChemicalShift,
+    SimpleFragment, StubFragment)
+from .structure.residue import (
+    AminoAcidResidue, get_all_residues, register_degenerate, register_residue)
 from .enzyme import Protease, cleave
-from .structure.glycan import HashableGlycanComposition
+from .structure.glycan import (
+    HashableGlycanComposition, GlycosylationType, TypedGlycanComposition)
+
+
+__all__ = [
+    "PeptideSequence", "ProteinSequence",
+    "AminoAcidResidue", "get_all_residues", "register_residue", "register_degenerate",
+    "Modification", "ModificationTable", "RestrictedModificationTable",
+    "ModificationRule", "ModificationTarget", "AnonymousModificationRule",
+    "IonSeries", "PeptideFragment", "ChemicalShift", "SimpleFragment", "StubFragment",
+    "Protease", "cleave",
+    "HashableGlycanComposition", "GlycosylationType", "TypedGlycanComposition",
+]
