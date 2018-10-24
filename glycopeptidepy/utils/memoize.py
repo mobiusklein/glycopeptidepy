49b4bf333d7657e1b05b9c761bd403f407e19207
Joshua Klein <mobiusklein@gmail.com>
2018-10-23 20:38:32 -0400

    Implement a class that injects caching of fragments

--- glycopeptidepy/utils/memoize.py
+++ glycopeptidepy/utils/memoize.py
@@ -19,3 +19,31 @@ def memoize(maxsize=100):
         func.memo = memo
         return func
     return deco
+
+
+class FragmentCachingMixin(object):
+    '''Implements a cache over get_fragments() that saves the last arguments and result
+    value, and returns it instead of recomputing result if the next invocation's arguments
+    match.
+    '''
+    def __init__(self, *args, **kwargs):
+        self.__fragments_value = None
+        self.__fragments_arguments = None
+        super(FragmentCachingMixin, self).__init__(*args, **kwargs)
+
+    def get_fragments(self, *args, **kwargs):
+        if self.__fragments_arguments == (args, kwargs):
+            return self.__fragments_value
+        else:
+            v = super(FragmentCachingMixin, self).get_fragments(*args, **kwargs)
+            self.__fragments_value = tuple(v)
+            self.__fragments_arguments = (args, kwargs)
+            return self.__fragments_value
+
+    def clear_fragments_cache(self):
+        self.__fragments_value = None
+        self.__fragments_arguments = None
+
+    @classmethod
+    def implement(cls, tp):
+        return type(tp.__name__, (cls, tp), {})
