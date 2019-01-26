6b68139e5963c7b468f0f54b99438c0f64a1f15f
Joshua Klein <mobiusklein@gmail.com>
2019-01-25 20:06:11 -0500

    Re-export more symbols

--- glycopeptidepy/structure/sequence/__init__.py
+++ glycopeptidepy/structure/sequence/__init__.py
@@ -1,7 +1,10 @@
 from .glycosylated_sequence import (
     find_glycosaminoglycan_sequons,
     find_n_glycosylation_sequons,
-    find_o_glycosylation_sequons)
+    find_o_glycosylation_sequons,
+    _n_glycosylation,
+    _o_glycosylation,
+    _gag_linker_glycosylation)
 
 from .implementation import (
     PeptideSequence,
@@ -18,4 +21,5 @@ __all__ = [
     "AnnotatedSequence", "find_glycosaminoglycan_sequons",
     "find_o_glycosylation_sequons", "find_n_glycosylation_sequons",
     "list_to_sequence", "parse",
+    "_n_glycosylation", "_o_glycosylation", "_gag_linker_glycosylation",
 ]
